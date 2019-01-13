7fb6cd50df53638b34436f6b91d0540ff6cadc00
Neal <winzipsdo@outlook.com>
2019-01-13 19:56:14 +0800

    redirect test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <p style="font-family: Serif">
+    <p class="img-wrapper" align="center">
+      <img src="@/assets/test1.png" />
+    </p>
+    <p class="img-wrapper" align="center">
+      <img src="@/assets/test2.png" />
+    </p>
+    <p style="width: 100%; text-align: center;">
       垂緌饮清露<br />
       流响出疏桐<br />
       居高声自远<br />
@@ -18,5 +24,11 @@ export default {
 </script>
 
 <style lang="stylus">
+  body
+    font-family Serif
 
+  .img-wrapper
+    img
+      display inline-block
+      width 500px
 </style>
