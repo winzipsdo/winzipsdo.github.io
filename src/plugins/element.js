683a4f60bd5e1413de66843b2f45b7461e85f1a2
Neal <winzipsdo@outlook.com>
2019-01-16 00:03:03 +0800

    to be a nerd

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import { Button, Input } from 'element-ui';
+import 'element-ui/lib/theme-chalk/base.css';
+import CollapseTransition from 'element-ui/lib/transitions/collapse-transition';
 
+Vue.component(CollapseTransition.name, CollapseTransition);
 Vue.use(Button);
 Vue.use(Input);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,88 @@
 <template>
-  <div>
-    <p class="img-wrapper" align="center">
-      <img src="@/assets/test1.png" />
-    </p>
-    <p class="img-wrapper" align="center">
-      <img src="@/assets/test2.png" />
-    </p>
+  <div class="container">
+    <!-- <el-button @click="handleGetRandomSong">test</el-button> -->
+    <transition name="el-fade-in-linear">
+      <div class="content-wrapper" v-show="state.contentShow">
+        <p v-for="(row, index) in currentSong.text" :key="index">{{row}}</p>
+        <p>{{currentSong.title}} - {{currentSong.author}}</p>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+const songs = require('../assets/data/songs.json');
+
 export default {
   name: 'home',
+  mounted() {
+    window.vue = this;
+    this.handleStartLoop();
+    window.addEventListener('blur', () => {
+      this.handleStopLoop();
+    });
+    window.addEventListener('focus', () => {
+      this.handleStartLoop();
+    });
+  },
+  data() {
+    return {
+      timer: '',
+      songs,
+      currentSong: songs[0],
+      state: {
+        contentShow: true,
+      },
+    };
+  },
+  methods: {
+    async sleep(ms) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    },
+    async handleGetRandomSong() {
+      this.state.contentShow = false;
+      await this.sleep(800);
+      const { length } = this.songs;
+      const index = Math.floor(Math.random() * length);
+      this.currentSong = this.songs[index];
+      this.state.contentShow = true;
+      console.log(new Date());
+    },
+    async handleStartLoop() {
+      if (this.timer === '') {
+        this.timer = setInterval(this.handleGetRandomSong, 5000);
+      }
+    },
+    async handleStopLoop() {
+      window.clearInterval(this.timer);
+      this.time = '';
+    },
+  },
 };
 </script>
 
 <style lang="stylus">
+
   body
     font-family Serif
+    display flex
+    flex-direction column
+    justify-content center
+    align-items center
+    height 90vh
 
   .img-wrapper
     img
       display inline-block
       width 500px
+
+  .content-wrapper
+    p
+      font-family Serif
 </style>
