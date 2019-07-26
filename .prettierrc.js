35242ccf51415a2ccb982aacd51eca2f489746eb
Neal <winzipsdo@outlook.com>
2019-07-26 16:27:53 +0800

    feat: add meta init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100, //换行的行长度
+  tabWidth: 2, //指定每个缩进级别的空格数
+  trailingComma: 'all', //多行使用拖尾逗号（默认none）
+  eslintIntegration: true, //开启 Eslint 检测支持
+  semi: true, //结尾不加分号
+  singleQuote: true, //使用单引号
+  jsxBracketSameLine: true, //将>多行JSX元素放在最后一行的末尾
+}
