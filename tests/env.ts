02cd0dbd5370308e2d1e790d1c34e10453308124
Neal <winzipsdo@outlook.com>
2019-08-14 11:07:58 +0800

    feat: share test

--- tests/env.ts
+++
@@ -1,4 +0,0 @@
-const ua_wechat =
-  'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1 wechatdevtools/1.02.1803210 MicroMessenger/6.5.7 Language/zh_CN webview/15638733648821498 webdebugger port/62125';
-const ua_qq =
-  'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 QQ/8.0.8.458 V1_IPH_SQ_8.0.8_1_APP_A Pixel/1125 Core/WKWebView Device/Apple(iPhone XS) NetType/WIFI QBWebViewType/1 WKType/1';
